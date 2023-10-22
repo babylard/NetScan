30323326dfabd85cd4f02740cb04ef342761a79e
William <75695872+babylard@users.noreply.github.com>
2023-10-21 19:22:25 -0700

    Commit 0.0.3 Source code

--- main.py
+++ main.py
@@ -1,40 +1,52 @@
 import scapy.all as scapy
-import re
-import datetime
 import itertools
 import threading
 import time
 import sys
-import os
-
+import manuf
 
 
 router = input("Input IP of the router you would like to scan: ")
 range = input("Range (24 is reccomended): ")
 done = False
 
-def animate():
-    for c in itertools.cycle(['|', '/', '-', '\\']):
-        if done:
-            break
-        sys.stdout.write('\rSending ARP requests (Takes longer with many devices on network)   ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\n\rDone     \nPress Enter to exit...')
-
-t = threading.Thread(target=animate)
+def get_device_type(mac_address):
+    p = manuf.MacParser()
+    manufacturer = p.get_manuf(mac_address)
+    return manufacturer or "Unknown"
 
 def scan(ip):
+    print("Sending ARP requests...")
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast = broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    print("\nIP\t\t\tMAC Address\n------------------------------------------")
+    
+    print("\nIP\t\tMAC Address\t\tManufacturer\t\tOS\n---------------------------------------------------------------------------------------------------------------")
+    
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        ip = element[1].psrc
+        mac = element[1].hwsrc
+        device_type = get_device_type(mac)
+        # Perform OS fingerprinting using SYN packets
+        try:
+            # Send SYN packet to port 80 (you can use other ports for more accuracy)
+            syn_packet = scapy.IP(dst=ip) / scapy.TCP(dport=80, flags="S")
+            response = scapy.sr1(syn_packet, timeout=1, verbose=False)
+            if response:
+                if response.haslayer(scapy.TCP) and response.getlayer(scapy.TCP).flags == 0x12:
+                    os_info = "Linux/Unix"
+                elif response.haslayer(scapy.TCP) and response.getlayer(scapy.TCP).flags == 0x14:
+                    os_info = "Windows"
+                else:
+                    os_info = "Unknown OS"
+            else:
+                os_info = "Unknown OS"
+        except:
+            os_info = "Error"
+        print(f"{ip}\t{mac}\t{device_type}\t{os_info}\t\t")
+    print("---------------------------------------------------------------------------------------------------------------")
 
-t.start()
 scan(router + "/" + range)
-done = True
-
-input("")
+print("Done")
+input("Press Enter to exit...")
